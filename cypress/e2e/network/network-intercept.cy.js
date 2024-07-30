e370fa79bc17005d766584aad448e2d34414a735
Madhurendra Nath Tiwari <madhurendraakela123@gmail.com>
2024-07-30 16:52:24 +0530

    Update network-intercept.cy.js

--- cypress/e2e/network/network-intercept.cy.js
+++ cypress/e2e/network/network-intercept.cy.js
@@ -1,6 +1,6 @@
 describe('Network Requests to JSONPlaceholder', () => {
   it('should intercept and stub the GET /educations request', () => {
-    // Stub the GET /educations request
+    
     cy.intercept('GET', 'https://madhurendra-portfolio-json-server.vercel.app/educations', {
       statusCode: 200,
       body: [
@@ -27,13 +27,13 @@ describe('Network Requests to JSONPlaceholder', () => {
       ]
     }).as('getEducationsStub');
 
-    // Visit the page that makes the network request
-    cy.visit('https://dev-madhurendra.vercel.app/'); // Replace with the actual route in your application
+   
+    cy.visit('https://dev-madhurendra.vercel.app/');
 
-    // Wait for the stubbed request and verify the response
+    
     cy.wait('@getEducationsStub').its('response.body').should('have.length', 2);
 
-    // Adjust selectors to match your page's structure
+   
     cy.get('button[role="tab"]').should('have.length', 2);
     cy.get('button[role="tab"]').first().invoke('text').then((text) => {
       expect(text.trim().toLowerCase()).to.equal('high school');
